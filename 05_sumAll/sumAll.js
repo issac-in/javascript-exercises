0c6ff8e6c7b3c33064f6922ef48075255071cdab
Issac In <66139562+issac-in@users.noreply.github.com>
2024-04-26 20:44:32 -0700

    Provide solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let isNaN = Number.isNaN(int1) || Number.isNaN(int2);
+    let isNotInteger = !(Number.isInteger(int1) && Number.isInteger(int2));
+    let isNegative = (int1 < 0) || (int2 < 0);
+    if (isNaN || isNotInteger || isNegative) { return "ERROR"; 
+    } else {
+        let min = (int1 < int2) ? int1 : int2;
+        let max = (int1 >= int2) ? int1 : int2;
+        let sum = 0;
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
