7410fc57facfa3613998df6b182ca04354ae0854
Issac In <66139562+issac-in@users.noreply.github.com>
2024-04-26 19:59:45 -0700

    Provide solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = "";
+    for (let i = -1; i >= -str.length; i--) {
+        reversed = reversed.concat(str.at(i));
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
