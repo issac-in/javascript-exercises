06c851673d3996644372edf8dc7527ffda1cf97b
Issac In <66139562+issac-in@users.noreply.github.com>
2024-04-26 20:22:45 -0700

    Provide solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...targets) {
+    for (let i = 0; i < inputArray.length; i++) {
+        for (let j = 0; j < targets.length; j++) {
+            if (inputArray[i] === targets[j]) {
+                inputArray.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
