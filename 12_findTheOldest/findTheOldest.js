7f2d7ce63c266235f8f4c78815dd47236eed2e92
Issac In <66139562+issac-in@users.noreply.github.com>
2024-05-07 08:16:55 -0700

    Provide solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        const year = new Date().getFullYear();
 
+        const oldestYoD = (oldest.yearOfDeath) ? oldest.yearOfDeath : year;
+        const currentYoD = (current.yearOfDeath) ? current.yearOfDeath : year;
+
+        const oldestAge = oldestYoD - oldest.yearOfBirth;
+        const currentAge = currentYoD - current.yearOfBirth;
+
+        return (oldestAge > currentAge) ? oldest : current;
+    }, {});
 };
 
 // Do not edit below this line
