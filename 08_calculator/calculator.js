fdefaebcd80c0ee48fda31c67ceadc1d9a964697
Issac In <66139562+issac-in@users.noreply.github.com>
2024-05-07 07:17:41 -0700

    Provide solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(operand1, operand2) {
+	return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(operand1, operand2) {
+	return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((sum,currentNum) => sum += currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((product, currentNum) => product *= currentNum, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i;    
+  }
+  return factorial;
 };
 
 // Do not edit below this line
