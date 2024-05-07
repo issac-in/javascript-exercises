9a37b948902216b66567318fe5221a68eadb83c0
Issac In <66139562+issac-in@users.noreply.github.com>
2024-05-07 07:41:15 -0700

    Provide solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (inputStr) {
+    const regexAnyNonWordCharacter = /\W/g;
+    const str = inputStr.toLowerCase().replace(regexAnyNonWordCharacter,"");
+    for (let i = 0; i < Math.trunc(str.length / 2); i++) {
+        if (str.at(i) !== str.at(str.length-1 - i)) { return false; } 
+    }
+    return true;
 };
 
 // Do not edit below this line
