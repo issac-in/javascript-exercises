f07e56f5c53c758c0c861e19de1efae0de3e3f61
Issac In <66139562+issac-in@users.noreply.github.com>
2024-05-07 08:43:09 -0700

    Review over solutions from 08 to 012 exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,12 @@
 const palindromes = function (inputStr) {
     const regexAnyNonWordCharacter = /\W/g;
     const str = inputStr.toLowerCase().replace(regexAnyNonWordCharacter,"");
-    for (let i = 0; i < Math.trunc(str.length / 2); i++) {
-        if (str.at(i) !== str.at(str.length-1 - i)) { return false; } 
-    }
-    return true;
+    // for (let i = 0; i < Math.trunc(str.length / 2); i++) {
+    //     if (str.at(i) !== str.at(str.length-1 - i)) { return false; } 
+    // }
+    // return true;
+    const reversStr = str.split("").reverse().join("");
+    return str === reversStr;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,27 @@
+// Recursion: Intuitive, naive approach. T(n) is 2**n. S(n) is n.
+// const fibonacci = function(n) {
+//     if (n < 0) { return "OOPS"; }
+//     else if (n == 0) { return 0; }
+//     else if (n == 1) { return 1; }
+//     else { return (fibonacci(n - 1) + fibonacci(n - 2)); }
+// };
+
+// Iterative: Less intuitive, faster. T(n) is n. S(n) is 1.
 const fibonacci = function(n) {
     if (n < 0) { return "OOPS"; }
     else if (n == 0) { return 0; }
-    else if (n == 1) { return 1; }
-    else { return (fibonacci(n - 1) + fibonacci(n - 2)); }
-};
+    else {
+        let firstPrev = 1; // F_n-1
+        let secondPrev = 0; // F_n-2
+
+        for (let i = 2; i <= n; i++) {
+            let current = firstPrev + secondPrev; // F_n = F_n-1 + F_n-2
+            secondPrev = firstPrev;
+            firstPrev = current;
+        }
+        return firstPrev;
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
